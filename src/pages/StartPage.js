2e1e9341e8fe5ec3abed3206a30d00f4b347c48b
domper <niquepersson@gmail.com>
2020-10-06 03:56:58 +0200

    added quote blocks

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -143,7 +143,7 @@ class StartPage extends Component {
                             <li>1M</li>
 
                         </ul> <ul>
-                            <p className="product-description"> 
+                            <p className="product-description">
                                 For a long, relaxing campground stay, a snug little backpacking tent just won’t do. Don't worry. We hear you.
                             </p>
                         </ul>
@@ -171,7 +171,7 @@ class StartPage extends Component {
                         </ul>
                         <p className="product-description"> <br></br>
                             Carp Fishing Full Kit 3 tips Carbon Fiber Feeder Fishing Rod Carp Fishing Reel and Fishing Carrier Bag</p>
-                            
+
                         <button class="btn btn-primary" >Book Now</button>
                     </div>
                 </div>
@@ -191,14 +191,14 @@ class StartPage extends Component {
                             <li>3D</li>
                             <li>1W</li>
 
-                        </ul> 
+                        </ul>
                         <ul >
-                        
-                        <p className="product-description"> <br></br>
-                            A successful camping trip starts with packing the right gear to help you brave the elements.
+
+                            <p className="product-description"> <br></br>
+                                A successful camping trip starts with packing the right gear to help you brave the elements.
                         </p>
                         </ul>
-                         <button class="btn btn-primary">Book Now</button>
+                        <button class="btn btn-primary">Book Now</button>
                     </div>
                 </div>
 
@@ -248,10 +248,36 @@ class StartPage extends Component {
         
             [32] But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? [33] On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammeled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse.
             </div> */}
-            <div>Boi</div>
-            </>
 
+
+        
+
+
+        <div class="blockquote-wrapper">
+            <div class="blockquote">
+                <h1>
+                    <br></br>We put the world at your feet.
+      
+                </h1>
+                <h4>- the Garage</h4>
+                <br></br>
+            </div>
+        </div>
+
+        <link href='https://fonts.googleapis.com/css?family=Josefin+Sans|Abril+Fatface' rel='stylesheet' type='text/css'/>
+
+    <blockquote1>
+        <p>
+            We make great outdoor experiences accessible to everyone by building a community 
+            of mindful adventurers who lend and borrow their gear in order 
+            to create a more sustainable product life cycle for the love of our environment.
+            </p>
             
+                <cite1><a href="https://www.brucelawson.co.uk/2013/on-citing-quotations-again/">The Garage</a></cite1>
+            
+            <div1></div1>
+    </blockquote1>
+            </>
 
         );
 
