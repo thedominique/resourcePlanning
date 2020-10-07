be47fc9257e3497ad037c120eaf05af7d7e52718
domper <niquepersson@gmail.com>
2020-10-07 20:10:32 +0200

    added buttons

--- src/pages/CommunityPage.js
+++ src/pages/CommunityPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './community.css'
 
 import Com from '../images/community.jpg'
+import Pals from '../images/pals.jpg'
 
 
 import { WithRouter } from 'react-router-dom'
@@ -14,49 +15,142 @@ class RentingPage extends Component {
         return (
             <>
                 <section class="hero">
-                    <div class="content">
-                        <h2>What are you waiting for?</h2>
-                        <p>
-                        Now that we took care of your gear it’s time to explore!
-                        Find your next tour guide, travel buddy or hiking companion in The Garage community.
+
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="content">
+                                    <h2>What are you waiting for?</h2>
+                                    <p color = "white">
+                                        Now that we took care of your gear it’s time to explore!
+                                        Find your next tour guide, travel buddy or hiking companion in The Garage community.
                         <br></br>
-                        Post your next adventure in the news feed and find likeminded people that will join you on your next trip.
+                                        Post your next adventure in the news feed and find likeminded people that will join you on your next trip.
                         </p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <link href='https://fonts.googleapis.com/css?family=Lato|Roboto:400,900' rel='stylesheet' type='text/css' />
+                                <div class="container">
+                                    
+                                    <div class="col">
+                                        <a href="http://marcel-pirnay.be/" class="btn">
+                                            <svg width="277" height="62">
+                                                <defs>
+                                                    <linearGradient id="grad1">
+                                                        <stop offset="0%" stop-color="#FF8282" />
+                                                        <stop offset="100%" stop-color="#E178ED" />
+                                                    </linearGradient>
+                                                </defs>
+                                                <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="240" height="50"></rect>
+                                            </svg>
+
+                                            <span>Climbing</span>
+                                        </a>
+                                    </div>
+
+                                    <a href="http://marcel-pirnay.be/" class="btn">
+                                        <svg width="277" height="62">
+                                            <defs>
+                                                <linearGradient id="grad1">
+                                                    <stop offset="0%" stop-color="#FF8282" />
+                                                    <stop offset="100%" stop-color="#E178ED" />
+                                                </linearGradient>
+                                            </defs>
+                                            <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="200" height="50"></rect>
+                                        </svg>
+
+                                        <span>skiing</span>
+                                    </a>
+
+                                    <a href="http://marcel-pirnay.be/" class="btn">
+                                            <svg width="277" height="62">
+                                                <defs>
+                                                    <linearGradient id="grad1">
+                                                        <stop offset="0%" stop-color="#FF8282" />
+                                                        <stop offset="100%" stop-color="#E178ED" />
+                                                    </linearGradient>
+                                                </defs>
+                                                <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="270" height="50"></rect>
+                                            </svg>
+
+                                            <span>Kayaking</span>
+                                        </a>
+
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                
+                                Tell us what you like
+                                <a href="http://marcel-pirnay.be/" class="btn">
+                                    <svg width="277" height="62">
+                                        <defs>
+                                            <linearGradient id="grad1">
+                                                <stop offset="0%" stop-color="#FF8282" />
+                                                <stop offset="100%" stop-color="#E178ED" />
+                                            </linearGradient>
+                                        </defs>
+                                        <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="180" height="50"></rect>
+                                    </svg>
+
+                                    <span>hiking</span>
+                                </a>
+
+                                <a href="http://marcel-pirnay.be/" class="btn">
+                                    <svg width="277" height="62">
+                                        <defs>
+                                            <linearGradient id="grad1">
+                                                <stop offset="0%" stop-color="#FF8282" />
+                                                <stop offset="100%" stop-color="#E178ED" />
+                                            </linearGradient>
+                                        </defs>
+                                        <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="270" height="50"></rect>
+                                    </svg>
+
+                                    <span>camping</span>
+                                </a>
+
+                                <a href="http://marcel-pirnay.be/" class="btn">
+                                    <svg width="277" height="62">
+                                        <defs>
+                                            <linearGradient id="grad1">
+                                                <stop offset="0%" stop-color="#FF8282" />
+                                                <stop offset="100%" stop-color="#E178ED" />
+                                            </linearGradient>
+                                        </defs>
+                                        <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="180" height="50"></rect>
+                                    </svg>
+
+                                    <span>more ></span>
+                                </a>
+
+                            </div>
+                        </div>
                     </div>
 
                 </section>
-
-
-                <div id="con">
-                    <h1></h1><br></br>
-                    <p>
-                        
-        
-            </p>
-                    <p>                Post your next adventure in the news feed and find likeminded people that will join you on your next trip.
-</p>
-
-                </div>
-
 
 
                 <div class="container">
                     <div class="row">
                         <div class="col-md-5">
                             <br></br><br></br><br></br><br></br>
+                            <img src={Com} width="600" height="400"></img>
+                        </div>
+                        <div class="col-md-4">
+                            <br></br><br></br><br></br><br></br>
                             <div class="blockquote-wrapper">
                                 <div class="blockquote">
                                     <h1>
                                         <br></br>We put the world at your feet.
-                            </h1>
+                                    </h1>
                                     <h4>- the Garage</h4>
                                     <br></br>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <br></br><br></br><br></br><br></br>
-                            <img src={Com} width="600" height="400"></img></div>
                     </div>
                 </div>
 
